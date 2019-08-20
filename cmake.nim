b57079fa0339ddf9790917bdf6619ae93558a47c
Ganesh Viswanathan <dev@genotrance.com>
2019-08-19 21:48:19 -0500

    Fix cmake dir location

--- cmake.nim
+++ cmake.nim
@@ -3,15 +3,15 @@ import os, osproc, strutils
 import nimterop/git
 
 const
-  baseDir = currentSourcePath.parentDir()/"cmake"
   version = "3.15.2"
+  url = "https://github.com/Kitware/CMake/releases/download/v$1/cmake-$1-win64-x64.zip" % version
 
 static:
-  downloadUrl("https://github.com/Kitware/CMake/releases/download/v$1/cmake-$1-win64-x64.zip" % version, baseDir)
+  url.downloadUrl(currentSourcePath.parentDir()/"cmake")
 
 when isMainModule:
   var
-    cmd = baseDir/"cmake-$1-win64-x64/bin/cmake" % version
+    cmd = getAppDir()/"cmake"/"cmake-$1-win64-x64/bin/cmake" % version
   for i in commandLineParams():
     cmd &= " " & i.quoteShell
 
